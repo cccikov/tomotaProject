860c7b8fed50fbcbe9a296b39b2b818b9e933359
cccikov <522978139@qq.com>
2016-12-07 16:51:37 +0800

    让我歇歇

--- web/js/createProject-1.js
+++ web/js/createProject-1.js
@@ -1,4 +1,26 @@
 $(function(){
     $(".scroll1").cccScrollBar();
     $(".scroll2").cccScrollBar();
+
+    $(".win-scroll").on("click","li",function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        var that = $(this);
+        if(!that.is(".active")){//非激活
+            that.addClass("active");
+        }else{
+            that.removeClass("active");
+        }
+    });
+
+    $(".toright").on("click",function(){
+        $(".scroll1 li.active").appendTo(".scroll2 ul").removeClass("active");
+        $(".scroll1").cccScrollBar();
+        $(".scroll2").cccScrollBar();
+    });
+    $(".toleft").on("click",function(){
+        $(".scroll2 li.active").appendTo(".scroll1 ul").removeClass("active");
+        $(".scroll1").cccScrollBar();
+        $(".scroll2").cccScrollBar();
+    });
 });
