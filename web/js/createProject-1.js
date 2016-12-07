40c2505e4a8788c8129dc5f8f909cba5b12a834d
cccikov <522978139@qq.com>
2016-12-07 17:56:46 +0800

    finish

--- web/js/createProject-1.js
+++ web/js/createProject-1.js
@@ -1,7 +1,35 @@
 $(function(){
-    $(".scroll1").cccScrollBar();
-    $(".scroll2").cccScrollBar();
+    // 待选管理员数据
+    var waitingData = [{"name":"admin01"},{"name":"admin02"},{"name":"admin03"},{"name":"admin04"},{"name":"admin05"},{"name":"admin06"},{"name":"admin07"},{"name":"admin08"},{"name":"admin09"},{"name":"admin10"},{"name":"admin11"},{"name":"admin12"},{"name":"admin13"},{"name":"admin14"},{"name":"admin15"},{"name":"admin16"},{"name":"admin17"},{"name":"admin18"},{"name":"admin19"},{"name":"admin20"},{"name":"admin21"},{"name":"admin22"},{"name":"admin23"},{"name":"admin24"},{"name":"admin25"},{"name":"admin26"},{"name":"admin27"},{"name":"admin28"},{"name":"admin29"},{"name":"admin30"},{"name":"admin31"},{"name":"admin32"},{"name":"admin33"},{"name":"admin34"},{"name":"admin35"},{"name":"admin36"},{"name":"admin37"},{"name":"admin38"},{"name":"admin39"},{"name":"admin40"},{"name":"admin41"},{"name":"admin42"},{"name":"admin43"},{"name":"admin44"},{"name":"admin45"},{"name":"admin46"},{"name":"admin47"},{"name":"admin48"},{"name":"admin49"},{"name":"admin50"}];
 
+    //管理员数据
+    var data = [{"id":51,"name":"admin51"},{"id":52,"name":"admin52"},{"id":53,"name":"admin53"},{"id":55,"name":"admin55"},{"id":56,"name":"admin56"},{"id":57,"name":"admin57"}];
+
+    // 生成待选管理员
+    function createWaitAdmin(data){
+        $(".scroll1 li").remove();
+        for(var i in data){
+            $('<li data-id="'+data[i].id+'">'+data[i].name+'</li>').appendTo(".scroll1 ul");
+        }
+    }
+    createWaitAdmin(waitingData);
+
+    // 生成管理员
+    function createAdmin(data){
+        $(".cp1-admin span").add(".scroll2 li").remove();
+        for(var i in data){
+            $('<span data-id="'+data[i].id+'">'+data[i].name+'<i>X</i></span>').insertBefore(".cp1-admin a");
+            $('<li data-id="'+data[i].id+'">'+data[i].name+'</li>').appendTo(".scroll2 ul");
+        }
+    }
+    createAdmin(data);
+
+
+
+
+
+    /*弹窗操作内 begin*/
+    //li的点击
     $(".win-scroll").on("click","li",function(e){
         e.stopPropagation();
         e.preventDefault();
@@ -12,8 +40,8 @@ $(function(){
             that.removeClass("active");
         }
     });
-
-    $(".toright").on("click",function(){
+    // 左右移动
+    $(".toright").on("click",function(){//因为不一定是会按确定，所以这些操作不改变管理员数据
         $(".scroll1 li.active").appendTo(".scroll2 ul").removeClass("active");
         $(".scroll1").cccScrollBar();
         $(".scroll2").cccScrollBar();
@@ -22,5 +50,82 @@ $(function(){
         $(".scroll2 li.active").appendTo(".scroll1 ul").removeClass("active");
         $(".scroll1").cccScrollBar();
         $(".scroll2").cccScrollBar();
+    });
+    /*弹窗操作内 end*/
+
+
+
+
+
+
+
+
+
+    /*弹窗本身操作*/
+    function openWin(){
+        $('<div class="mask" style="position: fixed;top:0;left:0;z-index:10;width:100%;height:100%;background: rgba(0,0,0,0.3)"></div>').appendTo('.wrap');
+        $(".cp1-win").addClass("active");
+        createAdmin(data);
+        createWaitAdmin(waitingData);
+
+        $(".scroll1").cccScrollBar();
+        $(".scroll2").cccScrollBar();
+    }
+    function closeWin(){
+        $(".mask").remove();
+        $(".cp1-win").removeClass("active");
+    }
+    $(".win-open").on("click",function(){
+        openWin();
+    });
+    $(".cp1-win-close").add(".cancel").on("click",function(){
+        closeWin();
+    });
+
+
+
+
+
+
+    /*数据操作*/
+    // 弹窗 确定按钮 数据操作
+    $(".sure").on("click",function(){
+         data = [];//清空数据
+         waitingData =[];//清空数据
+        $(".scroll2 li").each(function(item,key){//操作管理员数据
+            var obj = {};
+            obj.id = $(key).attr("id");
+            obj.name = $(key).html();
+            data.push(obj);
+        });
+        $(".scroll1 li").each(function(item,key){//操作待选管理员数据
+            var obj = {};
+            obj.id = $(key).attr("id");
+            obj.name = $(key).html();
+            waitingData.push(obj);
+        });
+        // console.log(data);
+        // console.log(waitingData);
+        createAdmin(data);
+        createWaitAdmin(waitingData);
+        closeWin();
+    });
+
+    //外面数据操作，外面管理员span的X,删除操作
+    $(".cp1-admin").on("click","i",function(){
+        var that = $(this).parent();
+        var id = that.attr("data-id");
+        var _index = -1;
+        for(var i in data){
+            if(data[i].id==id){
+                _index = i;
+            }
+        }
+        if(_index>-1){
+            var obj = data[_index];
+            data.splice(_index,1);
+            waitingData.push(obj);
+            that.remove();
+        }
     });
 });
