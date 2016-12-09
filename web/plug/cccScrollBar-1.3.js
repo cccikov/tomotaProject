3576845a81c7312d699c5a8e1af94d1a88759e24
cccikov <522978139@qq.com>
2016-12-09 11:00:57 +0800

    将jq插件方式修改为实例化方式, 以及将ccc这个具有作者特征的字符除去

--- web/plug/cccScrollBar-1.3.js
+++ web/plug/cccScrollBar-1.3.js
@@ -1,109 +1,126 @@
-$.fn.extend({
-	cccScrollBar: function() {
-		var outobj = $(this);
-		var inobj = outobj.find(".cccScrollContent").eq(0);
-		if(outobj.find(".sideBar").size()>0){
-			barobj = outobj.find(".sideBar");//为了重新建滚动，比如内容改变的时候
+function ScrollBar(ele){
+	this.name = "一个ScrollBar的实例";
+	this.init(ele);
+}
+ScrollBar.prototype = {
+	constructor:ScrollBar,
+	init:function(ele){
+
+		/*获取对象*/
+		var that = ele.css({"position":"relative","overflow":"hidden"});
+		var scrollWrap = that;//包含着滚动条的块
+		var scrollCtx = that.find(".cccScrollContent").eq(0);//滚动主体
+		if(scrollWrap.find(".slideBarY").size()>0){
+			var slideBarY = scrollWrap.find(".slideBarY");//为了重新建滚动，比如内容改变的时候
 		}else{
-			var barobj = $('<div class="sideBar"><div class="slider"></div></div>');
-			barobj.appendTo(outobj);
+			var slideBarY = $('<div class="slideBarY"><div class="sliderY"></div></div>');
+			slideBarY.appendTo(scrollWrap);
 		}
-		var sliderobj = barobj.find(".slider");
-
+		var sliderY = slideBarY.find(".sliderY");
 		var docu = $(document);
-		//移动方法
-		function goTopFn(top) {
-			$(this).css("transform", "translate(0," + top + "px");//不用3d是为了兼容ie9;
+		/*获取比例*/
+		var rateY = scrollWrap[0].scrollHeight / scrollWrap.innerHeight();//或者 var rateY = (scrollCtx.innerHeight()+(scrollWrap.innerHeight()-scrollWrap.height())/2) / scrollWrap.innerHeight();
+
+		/*将这些变量存入对象的属性里面,以便其他实例方法使用*/
+		this.option = {
+			"scrollWrap":scrollWrap,
+			"scrollCtx":scrollCtx,
+			"slideBarY":slideBarY,
+			"sliderY":sliderY,
+		}
+		this.rateY = rateY;
+
+		/*以下开始进行事件操作*/
+		if (rateY > 1) {
+            slideBarY.addClass("active");
+        }
+        sliderY.height(slideBarY.height() / rateY);
+
+
+        /*
+         * 点击
+         */
+        var self = this;//将实例对象用self存储起来,为了防止与事件里面的this冲突
+        slideBarY.off("mousedown").on("mousedown", function(e) {
+            var e = e || window.event;
+            var that = $(this);
+
+            //滑块偏移
+            var sliderTop = e.clientY - that.offset().top - slideBarY.height() / 2;
+            self.posiMove(sliderTop, true);
+        });
+
+
+        /*
+         * 滑动
+         */
+        sliderY.off("mousedown").on("mousedown", function(e) {
+            var e = e || window.event;
+            e.stopPropagation();
+            e.preventDefault();
+            var originHei = e.clientY - $(this).offset().top;
+
+            docu.on("mousemove", function(e) {
+                var e = e || window.event;
+
+                //滑块偏移
+                var sliderTop = e.clientY - slideBarY.offset().top - originHei;
+                self.posiMove(sliderTop, false);
+            });
+
+            docu.off("mouseup").on("mouseup", function() {
+                docu.off("mousemove");
+            });
+
+        });
+
+        /*
+         * 滚动 mousewheel--chrome ie; DOMMouseScroll--firefox
+         */
+        scrollWrap.off("mousewheel").off("DOMMouseScroll").on("mousewheel DOMMouseScroll", function(e) {
+            var e = e || window.event;
+            e.stopPropagation();
+            e.preventDefault();
+            var direction = "down";
+            if (e.originalEvent.wheelDelta < 0 || e.originalEvent.detail > 0) {
+                direction = "down";
+            } else {
+                direction = "up";
+            }
+
+            //滑块偏移
+            var sliderTop = sliderY.position().top;
+            if (direction == "down") {
+                sliderTop += 10;
+            } else {
+                sliderTop -= 10;
+            }
+            self.posiMove(sliderTop, false);
+        });
+
+	},
+	goTopFn:function(ele,top){//移动方法
+		ele.css("transform", "translate(0," + top + "px");//不用3d是为了兼容ie9;
+	},
+	posiMove:function(changeTop, animateFlag){//获取移动多少的方法，changeTop为滚动条滑块移动的距离，animateFlag是否要动画，并调用移动方法实现最终的滚动
+		var scrollCtx = this.option.scrollCtx;
+		var slideBarY = this.option.slideBarY;
+		var sliderY = this.option.sliderY;
+		var rateY = this.rateY;
+		//上下限
+		changeTop = changeTop >= 0 ? changeTop : 0;
+		changeTop = changeTop <= slideBarY.height() - sliderY.height() ? changeTop : slideBarY.height() - sliderY.height();
+
+		//内容偏移
+		var contentTop = -changeTop * this.rateY;
+
+		if (animateFlag) {
+		    scrollCtx.add(sliderY).addClass("animate");
+		} else {
+		    scrollCtx.add(sliderY).removeClass("animate");
 		}
 
-		//获取移动多少的方法，changeTop为滚动条滑块移动的距离，animateFlag是否要动画，并调用移动方法实现最终的滚动
-		function posiMove(changeTop, animateFlag) {
-			//上下限
-			changeTop = changeTop >= 0 ? changeTop : 0;
-			changeTop = changeTop <= barobj.height() - sliderobj.height() ? changeTop : barobj.height() - sliderobj.height();
-
-			//内容偏移
-			var contentTop = -changeTop * rate;
-
-			if (animateFlag) {
-				inobj.add(sliderobj).addClass("animate");
-			} else {
-				inobj.add(sliderobj).removeClass("animate");
-			}
-
-			goTopFn.apply(sliderobj, [changeTop]);
-			goTopFn.apply(inobj, [contentTop]);
-		}
-
-		/*
-		 * 比例
-		 */
-		var rate = outobj[0].scrollHeight / outobj.innerHeight();
-		//或者	var rate = (inobj.innerHeight()+(outobj.innerHeight()-outobj.height())/2) / outobj.innerHeight();
-		if (rate > 1) {
-			barobj.addClass("active");
-		}
-		sliderobj.height(barobj.height() / rate);
-
-		/*
-		 * 点击
-		 */
-		barobj.off("mousedown").on("mousedown", function(e) {
-			var e = e || window.event;
-			var that = $(this);
-
-			//滑块偏移
-			var sliderTop = e.clientY - that.offset().top - sliderobj.height() / 2;
-			posiMove(sliderTop, true);
-		});
-
-		/*
-		 * 滑动
-		 */
-		sliderobj.off("mousedown").on("mousedown", function(e) {
-			var e = e || window.event;
-			e.stopPropagation();
-			e.preventDefault();
-			var originHei = e.clientY - $(this).offset().top;
-
-			docu.on("mousemove", function(e) {
-				var e = e || window.event;
-
-				//滑块偏移
-				var sliderTop = e.clientY - barobj.offset().top - originHei;
-				posiMove(sliderTop, false);
-			});
-
-			docu.off("mouseup").on("mouseup", function() {
-				docu.off("mousemove");
-			});
-
-		});
-
-		/*
-		 * 滚动 mousewheel--chrome ie; DOMMouseScroll--firefox
-		 */
-		outobj.off("mousewheel").off("DOMMouseScroll").on("mousewheel DOMMouseScroll", function(e) {
-			var e = e || window.event;
-			e.stopPropagation();
-			e.preventDefault();
-			var direction = "down";
-			if (e.originalEvent.wheelDelta < 0 || e.originalEvent.detail > 0) {
-				direction = "down";
-			} else {
-				direction = "up";
-			}
-
-			//滑块偏移
-			var sliderTop = sliderobj.position().top;
-			if (direction == "down") {
-				sliderTop += 10;
-			} else {
-				sliderTop -= 10;
-			}
-			posiMove(sliderTop, false);
-		});
-
-		return outobj;
+		this.goTopFn(sliderY,changeTop);
+		this.goTopFn(scrollCtx,contentTop);
 	}
-});
+}
