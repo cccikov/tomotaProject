bec335ea814fba5d5ea428727d4d7ab2b3a1b555
cccikov <522978139@qq.com>
2016-12-19 15:41:31 +0800

    时间限制

--- web/js/timeLimit.js
+++ web/js/timeLimit.js
@@ -25,22 +25,19 @@ function isAllTrue(flagArr){
     };
 }
 
-var projectBeginTime;
-var projectEndTime;
+var projectBeginTime,projectEndTime;
 function projectTime(){
-    // Dec 23 2016 09:00:00
-    // Dec 30 2016 18:00:00
-
     /*项目时间限制*/
     var pb = $("#pb");
     var pe = $("#pe");
     var getExamBeginTime = new Date("Dec 23 2016 09:00:00");//后台获取考试时间-范围开始
     var getExamEndTime = new Date("Dec 30 2016 18:00:00");//后台获取考试时间-范围结束
+    projectBeginTime = getTime(pb.val());
+    projectEndTime = getTime(pe.val());
 
     pb.on("input",function(){
         var that = $(this);
         var val = getTime(that.val());//实时获取项目开始时间
-        projectEndTime = getTime(pe.val());//实时获取项目结束时间
         var projectStatus = $(".project-time input:radio:checked").val();
 
         /*判断部分*/
@@ -68,18 +65,15 @@ function projectTime(){
         }else{
             that.addClass("wrong");
             console.log(reason);
-            return false;
         }
 
         projectBeginTime = val;
         console.log("projectBeginTime : "+projectBeginTime+" \n "+new Date(projectBeginTime));
     });
 
-
     pe.on("input",function(){
         var that = $(this);
         var val = getTime(that.val());//实时获取项目结束时间
-        projectBeginTime = getTime(pb.val());//实时获取项目开始时间
         var projectStatus = $(".project-time input:radio:checked").val();
 
         /*判断部分*/
@@ -108,7 +102,6 @@ function projectTime(){
         }else{
             that.addClass("wrong");
             console.log(reason);
-            return false;
         }
 
         projectEndTime = val;
@@ -116,21 +109,49 @@ function projectTime(){
     });
 }
 
+function examTime(){
+    /*项目时间限制*/
+    var eb = $("#eb");
+    var ee = $("#ee");
+    var delay = $("#delay");
+    var duration = $("#duration");
+    var getNowTime = (new Date()).getTime()//后台获取当前时间
+    var examBeginTime = getTime(eb.val());
+    var examEndTime = getTime(ee.val());
+    var examDelayTime = getTime(delay.val());
+    var examDurationTime = getTime(duration.val());
+
+    eb.on("input",function(){
+        var that = $(this);
+        var val = getTime(that.val());//实时获取项目开始时间
+        var examStatus = $(".exam-time input:radio:checked").val();
+
+        /*判断部分*/
+        var arrNew = [
+
+        ];
+        var arrEdit = [
+
+        ];
+        var arr = projectStatus == "new" ? arrNew : arrEdit;
+
+        var flag = isAllTrue(arr).flag;
+        var reason = isAllTrue(arr).reason;
+        if(flag){
+            $("input").removeClass("wrong");
+        }else{
+            that.addClass("wrong");
+            console.log(reason);
+        }
+
+        projectBeginTime = val;
+        console.log("projectBeginTime : "+projectBeginTime+" \n "+new Date(projectBeginTime));
+    });
+}
 
 
 $(function(){
     tab();
     projectTime();
-
-
-
-
-
-
-
-    /*考试时间限制*/
-    var eb = $("#eb");
-    var ee = $("#ee");
-    var ebTime = 0;
-    var eeTime = 0;
+    examTime();
 });
